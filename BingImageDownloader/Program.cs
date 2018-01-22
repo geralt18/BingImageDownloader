5a9a8c462bbe4f2f8149ca8e8617c5f7eb40d32f
geralt18 <geralt18@gmail.com>
2018-01-22 18:36:00 +0100

    1. Refactoring 2. Changed image file name 3. Added ability to save download images history

--- BingImageDownloader/Program.cs
+++ BingImageDownloader/Program.cs
@@ -24,16 +24,20 @@ namespace BingImageDownloader
         [STAThread]
         static void Main()
         {
+            _logger.Info("Start");
+            _jsonFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, _jsonFileName);
             //AllocConsole();
-
             //Application.EnableVisualStyles();
             //Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new Form1());
+            LoadArchivedImages();
             GetJson();
-
+            SaveArchivedImages();
+            _logger.Info("Stop");
         }
 
         private const string _bingUrl = @"http://www.bing.com";
+        private const string _jsonFileName = "images.json";
 
         static void GetJson()
         {
@@ -41,32 +45,18 @@ namespace BingImageDownloader
             bool otherMarket = true;
             bool duplicates = false;
             List<BingImage> images = new List<BingImage>();
-
             String path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures), "Bing");
 
-            //var req = (HttpWebRequest)WebRequest.Create(GetUrl());
-            //var res = (HttpWebResponse)req.GetResponse();
-            //string json = new StreamReader(res.GetResponseStream()).ReadToEnd();
-            //Bing b = JsonConvert.DeserializeObject<Bing>(json);
-            //images.AddRange(b.images);
-
-            //if (missing)
-            //{
-            //    req = (HttpWebRequest)WebRequest.Create(GetUrl(7));
-            //    res = (HttpWebResponse)req.GetResponse();
-            //    json = new StreamReader(res.GetResponseStream()).ReadToEnd();
-            //    b = JsonConvert.DeserializeObject<Bing>(json);
-            //    images.AddRange(b.images);
-            //}
-
-            foreach (var market in markets)
+            foreach (var market in _markets)
             {
+                _logger.Info("Loading image list for {0}", market);
                 if (otherMarket)
                 {
                     var req = (HttpWebRequest)WebRequest.Create(GetUrl(0, 8, market));
                     var res = (HttpWebResponse)req.GetResponse();
                     var json = new StreamReader(res.GetResponseStream()).ReadToEnd();
                     var b = JsonConvert.DeserializeObject<Bing>(json);
+                    b.images.ForEach(x => x.market = market);
                     images.AddRange(b.images);
 
                     if (missing)
@@ -75,6 +65,7 @@ namespace BingImageDownloader
                         res = (HttpWebResponse)req.GetResponse();
                         json = new StreamReader(res.GetResponseStream()).ReadToEnd();
                         b = JsonConvert.DeserializeObject<Bing>(json);
+                        b.images.ForEach(x => x.market = market);
                         images.AddRange(b.images);
                     }
                 }
@@ -84,12 +75,15 @@ namespace BingImageDownloader
             foreach (var i in images)
             {
                 string url = string.Format("{0}{1}_{2}.jpg", _bingUrl, i.urlbase, GetSize());
-                DownloadFile(url, path, GetImageName(i, duplicates));
+                if (!_archivedImages.Exists(x => x.Name.Equals(i.GetImageName(), StringComparison.InvariantCultureIgnoreCase)))
+                {
+                    DownloadFile(url, path, i.GetImageFileName(duplicates));
+                    _archivedImages.Add(new ArchivedImage(i, null));
+                }
             }
 
             int index = new Random().Next(images.Count());
-            SetWallpaper(Path.Combine(path, CleanFileName(GetImageName(images[index], duplicates))));
-
+            SetWallpaper(Path.Combine(path, CleanFileName(images[index].GetImageFileName(duplicates))));
         }
 
         const int SPI_SETDESKWALLPAPER = 20;
@@ -102,6 +96,9 @@ namespace BingImageDownloader
 
         public static void SetWallpaper(string file)
         {
+            if (!File.Exists(file))
+                return;
+
             System.Drawing.Image img = System.Drawing.Image.FromFile(file);
             string tempPath = Path.Combine(Path.GetTempPath(), "wallpaper.bmp");
             img.Save(tempPath, System.Drawing.Imaging.ImageFormat.Bmp);
@@ -133,41 +130,24 @@ namespace BingImageDownloader
             return "1920x1080";
         }
 
-        static string GetImageName(BingImage i, bool dupl)
-        {
-            //EnglemannSpruceForest_ROW12845047121_1920x1080.jpg
-            string name = Path.GetFileName(i.url);
-            int index = name.IndexOf('_');
-            string imageName = name.Substring(0, index);
-            string rest = name.Substring(index + 1);
-            if (dupl)
-                return string.Format("{0}_{1}{2}", imageName, i.startdate, rest);
-            else
-                return string.Format("{0}_{1}{2}", imageName, i.startdate, Path.GetExtension(i.url));
-        }
+
 
         static void DownloadFile(string url, string path, string name)
         {
             string filePath = Path.Combine(path, CleanFileName(name?.Trim()));
             if (!Directory.Exists(path))
                 Directory.CreateDirectory(path);
-
-            //if (filePath.Length >= 250)
-            //   filePath = filePath.Substring(0, 249);
-
-            //filePath += ".mp3";
-
             try
             {
                 if (!File.Exists(filePath))
                 {
-                    _logger.Info("Pobieram {0}", url);
+                    _logger.Info("Donwloading {0}", url);
                     WebClient wb = new WebClient();
                     wb.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.33 Safari/537.36");
                     wb.DownloadFile(url, filePath);
                 }
                 else
-                    _logger.Info("Pomijam {0}", url);
+                    _logger.Info("Skipped {0}", url);
             }
             catch (Exception ex)
             {
@@ -193,8 +173,45 @@ namespace BingImageDownloader
             return string.Format(@"http://www.bing.com/HPImageArchive.aspx?format=js&idx={0}&n={1}&mkt={2}", index, count, market);
         }
 
-        static string[] markets = new string[12] { "EN-GB", "PT-BR", "EN-CA", "FR-CA", "EN-US", "EN-WW", "EN-AU", "JA-JP", "ZH-CN", "EN-IN", "DE-DE", "FR-FR" };
+        static void LoadArchivedImages()
+        {
+            if (!File.Exists(_jsonFilePath))
+                return;
+            _logger.Trace("Loading archived images list");
+            using (StreamReader file = File.OpenText(_jsonFilePath))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                _archivedImages = (List<ArchivedImage>)serializer.Deserialize(file, typeof(List<ArchivedImage>));
+            }
+        }
+
+        static void SaveArchivedImages()
+        {
+            _logger.Trace("Saving archived images list");
+            using (StreamWriter file = File.CreateText(_jsonFilePath))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Serialize(file, _archivedImages);
+            }
+        }
+
+        static string[] _markets = new string[12] {
+            "EN-US",
+            "DE-DE",
+            "EN-AU",
+            "EN-CA",
+            "EN-GB",
+            "EN-IN",
+            "EN-WW",
+            "FR-CA",
+            "FR-FR",
+            "JA-JP",
+            "PT-BR",
+            "ZH-CN" };
+
         private static Logger _logger = LogManager.GetCurrentClassLogger();
+        private static List<ArchivedImage> _archivedImages = new List<ArchivedImage>();
+        private static string _jsonFilePath;
 
         public class Bing
         {
@@ -221,7 +238,49 @@ namespace BingImageDownloader
             public string copyright { get; set; }
             public string copyrightlink { get; set; }
             public string quiz { get; set; }
+            public string market { get; set; }
+
+            public string GetImageName()
+            {
+                //EnglemannSpruceForest_ROW12845047121_1920x1080.jpg
+                string name = Path.GetFileName(this.url);
+                return name.Substring(0, name.IndexOf('_'));
+            }
+
+            public string GetImageFileName(bool dupl)
+            {
+                //EnglemannSpruceForest_ROW12845047121_1920x1080.jpg
+                string name = Path.GetFileName(this.url);
+                int index = name.IndexOf('_');
+                string imageName = name.Substring(0, index);
+                string rest = name.Substring(index + 1);
+                if (dupl)
+                    return string.Format("{0}_{1}{2}", this.startdate, imageName, rest);
+                else
+                    return string.Format("{0}_{1}_{2}{3}", this.startdate, imageName, this.market, Path.GetExtension(this.url));
+            }
         }
+
+        public class ArchivedImage
+        {
+            public string Name { get; set; }
+            public string Market { get; set; }
+            public string Description { get; set; }
+            public string Date { get; set; }
+            public string UrlBase { get; set; }
+
+            public ArchivedImage() { }
+
+            public ArchivedImage(BingImage b, string market)
+            {
+                Name = b.GetImageName();
+                Market = market;
+                Description = b.copyright;
+                Date = b.startdate;
+                UrlBase = b.urlbase;
+            }
+        }
+
 
     }
 }
